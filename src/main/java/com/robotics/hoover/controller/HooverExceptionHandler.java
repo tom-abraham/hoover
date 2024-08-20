1cb6b4ef579198638f70c8d00a0c543d59d64c77
tom-abraham <tomatriot@gmail.com>
2024-08-20 20:03:44 +0100

    Removed  unused exports and a toString method.

--- src/main/java/com/robotics/hoover/controller/HooverExceptionHandler.java
+++ src/main/java/com/robotics/hoover/controller/HooverExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.robotics.hoover.controller;
 
-import com.robotics.hoover.model.HooverResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
--- src/main/java/com/robotics/hoover/model/Coordinate.java
+++ src/main/java/com/robotics/hoover/model/Coordinate.java
@@ -11,7 +11,6 @@ import java.util.Objects;
 public class Coordinate {
     int x;
     int y;
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -19,18 +18,8 @@ public class Coordinate {
         Coordinate that = (Coordinate) o;
         return x == that.x && y == that.y;
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Coordinate{");
-        sb.append("x=").append(x);
-        sb.append(", y=").append(y);
-        sb.append('}');
-        return sb.toString();
     }
 }
